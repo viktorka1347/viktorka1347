88ef19de0eef81bba48a60e9b7b71720682c784c
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-10-02 14:57:35 +0300

    master
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
-import sum from './basic';
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+const game = new Game();
+game.start();
